0d99d3faebf6f9da235d4bae525a569d4576c02e
David Moreno Garcia <david.mogar@gmail.com>
2020-11-07 16:51:39 +0100

    Add security group

--- main.tf
+++ main.tf
@@ -11,7 +11,39 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "nginx" {
-  ami           = var.ami[var.region]
-  instance_type = var.instance
+resource "aws_security_group" "nginx" {
+  name        = "nginx"
+  description = "Allow traffic to http and ssh ports"
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+  }
 }
+
+resource "aws_instance" "nginx" {
+  ami                    = var.ami[var.region]
+  instance_type          = var.instance
+  vpc_security_group_ids = [aws_security_group.nginx.id]
+}
+
+resource "aws_eip_association" "nginx" {
+  allocation_id = aws_eip.nginx.id
+  instance_id   = aws_instance.nginx.id
+}
+
+resource "aws_eip" "nginx" {}
