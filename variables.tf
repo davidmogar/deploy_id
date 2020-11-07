3cb5ad6b5e613a5a44250e4f30a4225beaed2436
David Moreno Garcia <david.mogar@gmail.com>
2020-11-07 19:58:13 +0100

    Add variables descriptions

--- variables.tf
+++ variables.tf
@@ -2,29 +2,39 @@ variable "ami" {
   default = {
     "eu-west-3" = "ami-089d839e690b09b28"
   }
-  type = map
+  description = "Map containing the image id for each region"
+  type        = map
 }
 
 variable "instance" {
-  default = "t2.micro"
+  default     = "t2.micro"
+  description = "Intance type to use"
 }
 
 variable "region" {
-  default = "eu-west-3"
+  default     = "eu-west-3"
+  description = "Region in which to make the deployments"
 }
 
 variable "sg_egress_cidr" {
-  type = list
+  description = "CIDR to use for outgoing connections"
+  type        = list
 }
 
 variable "sg_ingress_cidr" {
-  type = list
+  description = "CIDR to use for incoming connections"
+  type        = list
 }
 
 variable "ssh_key_path" {
-  default = "~/.ssh/id_rsa"
+  default     = "~/.ssh/id_rsa"
+  description = "Path to the key to use for SSH connections"
 }
 
-variable "ssh_user" {}
+variable "ssh_user" {
+  description = "User to use for SSH connections"
+}
 
-variable "website_repository" {}
+variable "website_repository" {
+  description = "Path to the repository with the website to deploy in the webserver"
+}
