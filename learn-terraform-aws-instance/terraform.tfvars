6dc445ff6f702cba37bc3bc4a243495dc1853aae
David Moreno Garcia <david.mogar@gmail.com>
2020-11-07 13:02:52 +0100

    Move amis values into tfvars file

--- learn-terraform-aws-instance/terraform.tfvars
+++ learn-terraform-aws-instance/terraform.tfvars
@@ -1,1 +1,5 @@
+amis = {
+  "eu-west-2" = "ami-bd3622d9"
+  "eu-west-3" = "ami-050370d642c2f4559"
+}
 region = "eu-west-3"
--- learn-terraform-aws-instance/variables.tf
+++ learn-terraform-aws-instance/variables.tf
@@ -4,8 +4,4 @@ variable "region" {
 
 variable "amis" {
   type = map
-  default = {
-    "eu-west-2" = "ami-bd3622d9"
-    "eu-west-3" = "ami-050370d642c2f4559"
-  }
 }
