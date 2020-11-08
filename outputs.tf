a6cef42e5afbc25c58570634b6700e6aef85aad0
David Moreno Garcia <david.mogar@gmail.com>
2020-11-08 21:55:09 +0100

    Output SSH bastion public IP

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "bastion_public_ip" {
+  description = "SSH bastion's Public IP"
+  value       = aws_instance.bastion.public_ip
+}
+
 output "backend_private_ip" {
   description = "Private IP in front of the backend server"
   value       = aws_instance.backend.private_ip
