7523aab6d3a337ae2048f7f9688e38d78e707090
David Moreno Garcia <david.mogar@gmail.com>
2020-11-08 21:53:30 +0100

    Output backend server private and public IPs
    Private IP will be empty but is a way to see that it's getting deployed
    correctly.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "backend_private_ip" {
+  description = "Private IP in front of the backend server"
+  value       = aws_instance.backend.private_ip
+}
+
+output "backend_public_ip" {
+  description = "Public IP in front of the backend server"
+  value       = aws_instance.backend.public_ip
+}
+
 output "webserver_private_ip" {
   description = "Private IP in front of the webserver"
   value       = aws_instance.webserver.private_ip
