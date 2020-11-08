cab88e4282162b6d87dd4e16de6dd1a95f5d9c22
David Moreno Garcia <david.mogar@gmail.com>
2020-11-08 21:52:50 +0100

    Output webserver private and public IPs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
-output "nginx_ip" {
-  description = "Public IP in front of the NGINX server"
-  value       = aws_instance.nginx.public_ip
+output "webserver_private_ip" {
+  description = "Private IP in front of the webserver"
+  value       = aws_instance.webserver.private_ip
+}
+output "webserver_public_ip" {
+  description = "Public IP in front of the webserver"
+  value       = aws_instance.webserver.public_ip
 }
