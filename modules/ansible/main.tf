ca5da82970af69d982216428cd3b183df86d0055
David Moreno Garcia <david.mogar@gmail.com>
2020-11-09 19:49:12 +0100

    Move inventories to a directory

--- modules/ansible/main.tf
+++ modules/ansible/main.tf
@@ -2,7 +2,7 @@ resource "random_uuid" "inventory_name" {}
 
 resource "local_file" "inventory" {
   file_permission = "0440"
-  filename        = "ansible/${random_uuid.inventory_name.result}"
+  filename        = "ansible/inventories/${random_uuid.inventory_name.result}"
 
   content = <<-EOF
 [all:vars]
@@ -30,6 +30,6 @@ resource "null_resource" "provisioner" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ansible/${random_uuid.inventory_name.result} ${join(" ", compact(var.extra_arguments))} ${var.playbook}"
+    command = "ansible-playbook -i ansible/inventories/${random_uuid.inventory_name.result} ${join(" ", compact(var.extra_arguments))} ${var.playbook}"
   }
 }
