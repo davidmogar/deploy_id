e5d86118e0166d78f64d5fa260d38bf51bef90ee
David Moreno Garcia <david.mogar@gmail.com>
2020-11-10 17:08:23 +0100

    Generate letsencrypt and cloudflare origin ca certs

---
+++ certificates.tf
@@ -0,0 +1,34 @@
+resource "tls_private_key" "webserver" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "webserver" {
+  account_key_pem = tls_private_key.webserver.private_key_pem
+  email_address   = "null@${var.hostname.domain}"
+}
+
+resource "acme_certificate" "webserver" {
+  account_key_pem = acme_registration.webserver.account_key_pem
+  common_name     = "${var.hostname.subdomain}.${var.hostname.domain}"
+
+  dns_challenge {
+    provider = "cloudflare"
+  }
+}
+
+resource "tls_cert_request" "webserver" {
+  key_algorithm   = tls_private_key.webserver.algorithm
+  private_key_pem = tls_private_key.webserver.private_key_pem
+
+  subject {
+    common_name  = "${var.hostname.subdomain}.${var.hostname.domain}"
+    organization = "${var.hostname.domain}"
+  }
+}
+
+resource "cloudflare_origin_ca_certificate" "webserver" {
+  csr                = tls_cert_request.webserver.cert_request_pem
+  hostnames          = ["${var.hostname.subdomain}.${var.hostname.domain}"]
+  request_type       = "origin-rsa"
+  requested_validity = 365
+}
--- versions.tf
+++ versions.tf
@@ -4,5 +4,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    acme = {
+      source  = "terraform-providers/acme"
+      version = "~> 1.5"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.13"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
+    }
   }
+  required_version = ">= 0.13"
 }
