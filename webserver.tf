50d74c62e3c4e76506f3adb6868351bcef3170aa
David Moreno Garcia <david.mogar@gmail.com>
2020-11-11 21:06:52 +0100

    Add webserver autoscaling based on memory

--- webserver.tf
+++ webserver.tf
@@ -34,3 +34,55 @@ module "webserver" {
   ))
   vpc = aws_vpc.main
 }
+
+resource "aws_autoscaling_policy" "instances_scale_up" {
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = module.webserver.autoscaling_group.name
+  cooldown               = 300
+  name                   = "instances_scale_up"
+  scaling_adjustment     = 1
+}
+
+resource "aws_autoscaling_policy" "instances_scale_down" {
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = module.webserver.autoscaling_group.name
+  cooldown               = 300
+  name                   = "instances_scale_down"
+  scaling_adjustment     = -1
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_high" {
+  alarm_actions = [
+    aws_autoscaling_policy.instances_scale_up.arn
+  ]
+  alarm_description   = "Metric to monitor EC2 instnaces memory for high utilization"
+  alarm_name          = "mem_util_high_instances"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  dimensions = {
+    AutoScalingGroupName = module.webserver.autoscaling_group.name
+  }
+  evaluation_periods = "2"
+  metric_name        = "MemoryUtilization"
+  namespace          = "System/Linux"
+  period             = "300"
+  statistic          = "Average"
+  threshold          = "80"
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_low" {
+  alarm_actions = [
+    aws_autoscaling_policy.instances_scale_down.arn
+  ]
+  alarm_description   = "Metric to monitor EC2 instnaces memory for low utilization"
+  alarm_name          = "mem_util_low_instances"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  dimensions = {
+    AutoScalingGroupName = module.webserver.autoscaling_group.name
+  }
+  evaluation_periods = "2"
+  metric_name        = "MemoryUtilization"
+  namespace          = "System/Linux"
+  period             = "300"
+  statistic          = "Average"
+  threshold          = "40"
+}
