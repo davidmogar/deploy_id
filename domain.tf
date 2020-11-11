890bc21def8371533d940b05dafe859bcebcb72d
David Moreno Garcia <david.mogar@gmail.com>
2020-11-11 23:01:46 +0100

    Use a non proxied record

--- domain.tf
+++ domain.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "subdomain" {
   name    = var.hostname.subdomain
-  proxied = true
+  proxied = false
   type    = "CNAME"
   value   = module.webserver.loadbalancer_dns_name
   zone_id = var.cloudflare_zone_id
